7fe8a5b7307c4103ff5ddf869caa6b0444310fae
Igor Snitkin <djshked@gmail.com>
2019-01-17 23:44:59 +0100

    Implement permutations algorithm. NOTE: Removing duplicates step is incorrect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-'use strict';
-
-function permutations(string) {
-  let result = [];
-  // your code here please
-  return result;
-}
+const permutations = (str) => {
+  const result = [];
+  if (str.length < 2) return [str];
+  [...str].forEach((char, i) => {
+    const rest = str.substring(0, i) + str.substring(i + 1);
+    permutations(rest).forEach(p => result.push(char + p));
+  });
+  return [...new Set(result)];
+}; 
 
 module.exports = permutations;
